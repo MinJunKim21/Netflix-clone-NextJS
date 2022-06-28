5a563069c263058d99868d8c3b18ed93dcbde854
MinJunKim21 <jinju8012@gmail.com>
2022-06-28 16:22:47 +0900

    finisehd react-hook-form, starting login function

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,28 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { SubmitHandler, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 interface Inputs {
   email: string;
   password: string;
 }
+
 function Login() {
   const [login, setLogin] = useState(false);
+
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = ({ email, password }) => {
+    if (login) {
+      // await singIn(email, password)
+    } else {
+      // await signUp(email, password)
+    }
+  };
 
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
@@ -50,6 +57,11 @@ function Login() {
               className="input"
               {...register('email', { required: true })}
             />
+            {errors.email && (
+              <p className=" p-1 text-[13px] font-light text-orange-500 ">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
           <label className="inline-block w-full">
             <input
@@ -58,16 +70,28 @@ function Login() {
               className="input"
               {...register('password', { required: true })}
             />
+            {errors.password && (
+              <p className=" p-1 text-[13px] font-light text-orange-500 ">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
 
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}
+        >
           Sign In
         </button>
 
         <div className="text-[gray]">
           New to Netflix?{' '}
-          <button type="submit" className=" text-white hover:underline">
+          <button
+            type="submit"
+            className=" text-white hover:underline"
+            onClick={() => setLogin(false)}
+          >
             Sign up now
           </button>
         </div>
