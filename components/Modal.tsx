dfdcd57d7c62d2984dbc944bc1f211a57ad21dce
MinJunKim21 <jinju8012@gmail.com>
2022-07-01 19:59:59 +0900

    made modal x button, starting react player

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,3 +1,4 @@
+import { XIcon } from '@heroicons/react/solid';
 import MuiModal from '@mui/material/Modal';
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtoms';
@@ -11,7 +12,15 @@ function Modal() {
 
   return (
     <MuiModal open={showModal} onClose={handleClose}>
-      <>Modal</>
+      <>
+        <button
+          onClick={handleClose}
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+        >
+          <XIcon className="h-6 w-6" />
+        </button>
+      </>
+      {/* 이 빈 괄호는 mui의 modal 사용법에서 한번 감싸주어야 되는 실행법이 있어서 형식적으로 만든거임 */}
     </MuiModal>
   );
 }
